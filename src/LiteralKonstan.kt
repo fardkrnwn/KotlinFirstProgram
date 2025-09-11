a2b49a01b21ffe6966faea1ad4bf883a6d8755f4
fardkrnwn <mhdf045@gmail.com>
2025-09-11 21:26:59 +0700

    Percobaan Menggunakan Literal Konstan

---
+++ src/LiteralKonstan.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val floatLiteral: Float = 178.95f //Literal Float
+    println("Contoh Literal Float = " + floatLiteral)
+    val oneBillion: Long = 1000000000L //Literal long
+    println("Contoh Literal Long = " + oneBillion)
+    val valHeksa = 0x0f //Literal heksodesimal diawali dengan 0x
+    println("Contoh Literal Heksadesimal = " + valHeksa)
+    val valBinary = 0b00001011 //Literal Binary diawali dengan 0b
+    println("Contoh Liberal Binary = " + valBinary)
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional Floating Point Number = " + notasiKonvensional)
+}
